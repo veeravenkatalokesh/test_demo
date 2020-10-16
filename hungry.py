ee08d30160720ae8665430666ebba436b2661f9d
unknown <lokesh.chowdary777@gmail.com>
2020-10-15 22:16:17 -0400

    hungry.py

--- hungry.py
+++ hungry.py
@@ -1,2 +1,7 @@
-i am eating pizza
-i am eating burger
+hungry=input("are you hungry")
+if hunhry=="yes":
+	print("eat pizza")
+else:
+	thirsty=input("are you thirsty?")
+	if thirsty=="yes":
+		print("drinkwater")
